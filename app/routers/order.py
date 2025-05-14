f9e13573882f2e48b5b37d659c28691595996c12
Chinthana Rajapaksha <105596929+Chinthana7858@users.noreply.github.com>
2025-05-14 14:37:14 +0530

    Update order.py

--- app/routers/order.py
+++ app/routers/order.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.auth.utils import require_admin
 from app.db import get_db
-from app.schemas.order import OrderCreate, OrderItemCreate, OrderOut
-from app.services.order_service import create_order, create_order_item, fetch_all_orders
+from app.schemas.order import OrderCreate, OrderItemCreate, OrderOut, PaginatedOrders
+from app.services.order_service import create_order, create_order_item, fetch_all_orders, fetch_all_orders_paginated
 from app.services.user_service import get_orders_by_user
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
